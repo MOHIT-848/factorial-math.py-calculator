24360b42b6d98d59177dc4d7018d5d94d5ea7f5c
MOHIT-848 <singhsaabh1548@gmail.com>
2025-05-09 17:17:08 +0530

    Add files via upload

---
+++ cacl.py
@@ -0,0 +1,6 @@
+# calculator using math.py
+import math
+n= int(input("enter the number="))
+print("square root of number is",math.sqrt(n))
+print("log of number is",math.log(n))
+print("sine of the number is ",math.sin(n))
